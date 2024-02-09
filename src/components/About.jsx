a7d376bac06cdd8ff90d56826676e46c8d513440
italonrg <italonrg500@gmail.com>
2024-02-09 16:30:32 -0300

    About Commit ³

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,63 @@
 import React from "react";
-
+import aboutImg from '../assets/images/about.png'
 const About = () => {
-  return <div>About</div>;
+
+  const info = [
+    {text:'Tempo de experiência' , count: '??'},
+    {text:'Projetos concluídos' , count: '??'},
+    {text:'Horas estudadas' , count: '??'},
+  ]
+  return <section id="about" className="py-10 text-white">
+
+
+    <div className="text-center mt-8">
+    <h3 className=" text-4xl  font-semibold ">
+      Sobre <span className="text-cyan-600 ">Min</span>
+    </h3>
+
+    <p className=" text-gray-600 my-3 text-lg">Minha historia</p>
+    <div className=" flex md:flex-row flex-col-reverse items-center md:gap-6 gap-12 px-10 max-w-6xl mx-auto">
+      
+  <div className=" p-2">
+    <div className=" text-gray-400 my-3">
+      <p className=" text-justify leading-7 w-11/12 mx-auto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure cum ipsa cupiditate necessitatibus inventore ea sit eum perspiciatis nam iste quo labore perferendis dolorum officiis eos, quas asperiores totam aliquid!
+
+      </p>
+      <div>
+          <div className=" flex mt-10 items-center gap-7">
+            {
+              info.map(content => (
+                <div key={content.text}>
+                      <h3 className=" md: text-4xl font-semibold text-white">
+                        {content.count}
+                       <span className=" text-cyan-600 ">+</span> 
+                       </h3>
+                      <span className=" md:text-base text-xs ">{content.text}</span>
+                    </div>
+              ))}
+        </div>
+        <br />
+        <br />
+              <a href="./src/assets/italonascimento.PDF" download>
+              <button className=" btn-primary">Baixar Currículo</button>
+              </a>
+      </div>
+    </div>
+  </div>
+  <div className=" flex-1 flex justify-center items-center">
+
+    <div className=" lg: w-96 h-full relative sm:w-10/12-w-10/12 max-w-sm bg-cyan-600 rounded-xl aboutImg">
+      <img
+       src={aboutImg}
+        alt=""
+         classname=" w-full object-cover rounded-xl"/>
+    </div>
+
+  </div>
+
+    </div>
+    </div>
+  </section>;
 };
 
 export default About;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React from "react";///eu tenh que gerar uma foto minha
+import hero from '../assets/images/hero.png'
 
 const Hero = () => {
-  return <div className="h-screen">Hero</div>;
+
+  const social_media =[
+    'logo-instagram',
+    'logo-facebook',
+    'logo-linkedin',
+    'logo-twitter',
+  ]
+  return (
+    <section 
+  id="home"
+  className=" min-h-screen flex py-10 md:flex-row flex-col items-center"
+  >
+    <div className="flex-1 flex items-center justify-center">
+      <img src={hero} alt="" className="md:w-11/12 h-full object-cover" />
+    </div>
+
+    <div className="flex-1 ">
+
+    </div>
+    <div>
+      <h1 className="md:text-5xl text-2xl md:leading-normal leading-10 text-white font-bold">
+        <span className="text-cyan-600 md: text-6xl-text-5xl">
+          Oi!
+          <br />
+        </span>
+        Meu nome e <span>Ítalo Nascimento</span>
+      </h1>
+      <h4 className="md: text-2xl-text-lg md: leading-normal-leading-5 mt-4 font-bold text-gray-600">Desenvolvedor Fullstack</h4>
+      <button className="btn-primary mt-8">Contato</button>
+      <div className="mt-8 text-3xl flex items-center md:justify-start justify-center "></div>
+      <div>
+
+        {
+          social_media?.map(icon=>(
+            <div key={icon} className="text-gray-600 hover:text-white cursor-pointer">
+              <ion-icon name={icon}></ion-icon>  
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  </section>
+  );
 };
 
 export default Hero;
